5eda6a5ab79501234cad27ecb02d957ca029c087
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-05-07 04:12:54 -0500

    states with class

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
 import "./styles/BadgeNew.css";
-import Navbar from "../components/Navbar";
 import logo from "../images/logo.svg";
+import Navbar from "../components/Navbar";
 import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends Component {
+  state = {
+    form: { firstName: "", lastName: "", email: "", jobTitle: "", twitter: "" },
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
   render() {
     return (
       <div>
@@ -14,12 +28,19 @@ class BadgeNew extends Component {
         </div>
         <div className="container">
           <div className="row">
-            <div className="col">
+            <div className="col-6">
               <Badge
-                firstName="Edilberto"
-                lastName="Vazquez"
-                jobTitle="Systems and IT engineer"
-                twitter="No tengo Twitter"
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                email={this.state.form.email}
+                jobTitle={this.state.form.jobTitle}
+                twitter={this.state.form.twitter}
+              />
+            </div>
+            <div className="col-6">
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
               />
             </div>
           </div>
