028581305ec92e88a589ae16b1f7f88fc5fde3b3
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-05-08 00:15:59 -0500

    Badges page created

---
+++ src/components/BadgesList.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./styles/BadgesList1.css";
+import avatar from "../images/twitter.svg";
+
+class BadgesList extends Component {
+  render() {
+    return (
+      <div>
+        <ul className="list-unstyled">
+          {this.props.badges.map((badge) => {
+            return (
+              <li key={badge.id}>
+                <div className="Badges-co-avatar">
+                  <img src={badge.avatarUrl} alt="" />
+                </div>
+                <div className="Badges-co-user-info">
+                  <p className="Badges-co-user-info__Name">
+                    {badge.firstName} {badge.lastName}
+                  </p>
+                  <p className="Badges-co-user-info__twitter">
+                    <img src={avatar} alt="" /> @{badge.twitter}
+                  </p>
+                  <p className="Badges-co-user-info__JobTitle">
+                    {badge.jobTitle}
+                  </p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BadgesList;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
 import BadgeNew from "./pages/BadgeNew";
+import Badges from "./pages/Badges";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BadgeNew />
+    <Badges />
   </React.StrictMode>,
   document.getElementById("root")
 );
